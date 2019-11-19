f55b84594ea31b6914b6f53a4ae4eef2b01408cc
konix <1546500177@qq.com>
2019-11-20 07:35:36 +0800

    java流程控制

--- start/src/main/java/com/rjf/basic/Arr.java
+++ start/src/main/java/com/rjf/basic/Arr.java
@@ -6,8 +6,15 @@ public class Arr {
     public static void main(String[] args){
         int[] as={1,3,5,7,9};
         int[] as1=new int[5];
+        int[] as2={2,8,1,6,4,3,9,0,7,5};
+
         as1=as;
         System.out.println(Arrays.toString(as1));
+
+        Arrays.sort(as2);
+        for (int i:as2 ) {
+            System.out.println(i);
+        }
 
         String[] names={"xiaoming","xiaohua","xiaohong"};
         System.out.println(names[0]);
---
+++ start/src/main/java/com/rjf/process/If.java
@@ -0,0 +1,12 @@
+package com.rjf.process;
+
+public class If {
+    public static void main(String[] args){
+        int n=70;
+        if (n>=60){
+            System.out.println("及格了");
+        }else{
+            System.out.println("不及格");
+        }
+    }
+}
