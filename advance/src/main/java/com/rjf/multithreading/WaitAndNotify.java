c374198dda8561c11f56339e8a1feb15f352dca4
konix <1546500177@qq.com>
2019-12-10 21:21:17 +0800

    wait & notifyAll

--- advance/src/main/java/com/rjf/multithreading/WaitAndNotify.java
+++ advance/src/main/java/com/rjf/multithreading/WaitAndNotify.java
@@ -65,3 +65,17 @@ class TaskQueue{
         return queue.remove();
     }
 }
+/*
+
+        wait和notify用于多线程协调运行：
+
+        在synchronized内部可以调用wait()使线程进入等待状态；
+
+        必须在已获得的锁对象上调用wait()方法；
+
+        在synchronized内部可以调用notify()或notifyAll()唤醒其他等待线程；
+
+        必须在已获得的锁对象上调用notify()或notifyAll()方法；
+
+        已唤醒的线程还需要重新获得锁后才能继续执行。
+*/
