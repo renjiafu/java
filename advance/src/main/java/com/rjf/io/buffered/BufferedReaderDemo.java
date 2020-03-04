db9444437bb0bfa71177ff3b170df397aab30640
Rene <1546500177@qq.com>
2020-03-04 22:55:48 +0800

    BufferedIO

---
+++ advance/src/main/java/com/rjf/io/buffered/BufferedReaderDemo.java
@@ -0,0 +1,16 @@
+package com.rjf.io.buffered;
+
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+public class BufferedReaderDemo {
+
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new FileReader("a.txt"));
+        System.out.println(br.readLine());
+
+        br.close();
+    }
+}
---
+++ advance/src/main/java/com/rjf/io/buffered/BufferedWriterDemo.java
@@ -0,0 +1,16 @@
+package com.rjf.io.buffered;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class BufferedWriterDemo {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedWriter bw = new BufferedWriter(new FileWriter("a.txt"));
+        bw.write("Rene");
+
+        bw.close();
+    }
+}
