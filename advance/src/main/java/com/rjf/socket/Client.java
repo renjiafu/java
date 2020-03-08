624bc7bd61ee07bcca8551d08e7e0199f7dfb09e
Rene <1546500177@qq.com>
2020-03-08 15:22:05 +0800

    Lambda表达式

--- advance/src/main/java/com/rjf/socket/Client.java
+++ advance/src/main/java/com/rjf/socket/Client.java
@@ -5,28 +5,28 @@ import java.net.Socket;
 import java.util.Scanner;
 
 public class Client {
-   public  static void main(String[]args){
+
+   public  static void main(String[] args){
+
      String serverName="localhost";
      int port=6666;
      try {
          System.out.println("连接主机:" + serverName + "端口号:" + port);
-         Socket client = new Socket(serverName, port);
-         System.out.println("主机地址:"+client.getRemoteSocketAddress());
+         Socket clientSocket = new Socket(serverName, port);
+         System.out.println("主机地址:"+clientSocket.getRemoteSocketAddress());
 
-         OutputStream os=client.getOutputStream();
+         OutputStream os=clientSocket.getOutputStream();
          DataOutputStream dos=new DataOutputStream(os);
          Scanner sc=new Scanner(System.in);
-         String str=null;
+         String str = null;
          str=sc.nextLine();
-         dos.writeUTF("Hello ,i'm "+str+client.getLocalSocketAddress());
-
-         InputStream is=client.getInputStream();
+         dos.writeUTF("Hello ,i'm "+str+clientSocket.getLocalSocketAddress());
+         InputStream is=clientSocket.getInputStream();
          DataInputStream dis=new DataInputStream(is);
          System.out.println("服务器响应:"+dis.readUTF());
          System.out.println("服务器响应:"+dis.readUTF());
 
-
-         client.close();
+         clientSocket.close();
      }catch(IOException e){
          e.printStackTrace();
      }
--- advance/src/main/java/com/rjf/socket/Server.java
+++ advance/src/main/java/com/rjf/socket/Server.java
@@ -9,6 +9,7 @@ import java.net.SocketException;
 import java.util.Scanner;
 
 public class Server extends Thread{
+
     private ServerSocket ss;
     public Server(int port)throws IOException{
         ss=new ServerSocket(port);
@@ -18,33 +19,30 @@ public class Server extends Thread{
         while (true){
             try {
                 System.out.println("等待远程连接,端口为:" + ss.getLocalPort() + "...");
-                Socket server = ss.accept();
-                System.out.println("服务器地址" + server.getRemoteSocketAddress());
+                Socket serverSocket = ss.accept();
+                System.out.println("服务器地址" + serverSocket.getRemoteSocketAddress());
 
-                DataInputStream dis=new DataInputStream(server.getInputStream());
+                DataInputStream dis=new DataInputStream(serverSocket.getInputStream());
                 System.out.println(dis.readUTF());
 
-                DataOutputStream dos=new DataOutputStream(server.getOutputStream());
-                dos.writeUTF("您已连接server"+server.getLocalSocketAddress());
+                DataOutputStream dos=new DataOutputStream(serverSocket.getOutputStream());
+                dos.writeUTF("您已连接server"+serverSocket.getLocalSocketAddress());
                 Scanner sc = new Scanner(System.in);
                 dos.writeUTF("服务器回复"+sc.nextLine());
-                server.close();
-
+                serverSocket.close();
 
             }catch(SocketException s){
                 System.out.println("连接超时!");
             }catch(IOException e){
               e.printStackTrace();
             }
-
         }
     }
 
     public static void main(String[] args){
         int port=6666;
         try{
-            Thread t=new Server(port);
-            t.run();
+            new Server(port).start();
         }catch(IOException e){
             e.printStackTrace();
         }
