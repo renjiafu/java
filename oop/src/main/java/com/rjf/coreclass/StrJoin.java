9ea7f1586b923175c07a326d942e3b97200c9945
Rene <1546500177@qq.com>
2020-03-16 22:14:44 +0800

    String

--- oop/src/main/java/com/rjf/coreclass/StrJoin.java
+++
@@ -1,9 +0,0 @@
-package com.rjf.coreclass;
-
-public class StrJoin {
-    public static void main(String[] args){
-        String s="Hello";
-        String.join(s,"World");
-        System.out.println(s);
-    }
-}
---
+++ start/src/main/java/com/rjf/common/StringDemo.java
@@ -0,0 +1,16 @@
+package com.rjf.common;
+
+public class StringDemo {
+
+    public static void main(String[] args) {
+
+        String str = "Hello Rene! ";
+
+        System.out.println(str);
+        System.out.println(str.length());
+        System.out.println(str.charAt(0));
+        System.out.println(str.replace('R','K'));
+        System.out.println(str.hashCode());
+
+    }
+}
