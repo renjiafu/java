5fed89b40bcc44f4853bdcce1a072ed936a9cc47
Rene <1546500177@qq.com>
2020-05-06 23:01:41 +0800

    java基础 - 变量类型

--- start/src/main/java/com/rjf/basic/BasicVariablesDemo.java
+++ start/src/main/java/com/rjf/basic/BasicVariablesDemo.java
@@ -1,4 +1,26 @@
 package com.rjf.basic;
 
+/*
+ *
+ *   Rene
+ *   2019/6/6 22:54
+ */
+
 public class BasicVariablesDemo {
+
+    // 静态的私有变量
+    private static double salary;
+
+    // 常量
+    public static final String DEV = "开发者";
+
+    // 私有变量
+    private String name;
+
+    public static void main(String[] args) {
+        // 局部变量
+        int age = 18;
+        salary = 10000;
+        System.out.println(DEV+"薪水一般为: "+salary);
+    }
 }
