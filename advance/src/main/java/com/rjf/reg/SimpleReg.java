02c61d86ca0ef15c9983082d7300f85e8e315d24
Rene <1546500177@qq.com>
2020-05-11 22:47:43 +0800

    java基础- 正则

--- advance/src/main/java/com/rjf/reg/SimpleReg.java
+++ advance/src/main/java/com/rjf/reg/SimpleReg.java
@@ -3,18 +3,21 @@ package com.rjf.reg;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class SimpleRegular {
+public class SimpleReg {
+
     public static void main(String[] args){
+
         Pattern pattern=Pattern.compile("(\\d{3,4})\\-(\\d{7,8})");
         Matcher matcher=pattern.matcher("010-12345678");
+
         if (matcher.matches()){
             String s1= matcher.group(1);
             String s2= matcher.group(2);
 
             System.out.println(s1+"\n"+s2);
-
         }else {
             System.out.println("fail!");
         }
+
     }
 }
