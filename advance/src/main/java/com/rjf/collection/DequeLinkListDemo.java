9a1b056d1941a85a81e05906bb3440b79882d7d7
Rene <1546500177@qq.com>
2020-05-24 21:46:08 +0800

    java基础 - 集合

--- advance/src/main/java/com/rjf/collection/DequeLinkListDemo.java
+++ advance/src/main/java/com/rjf/collection/DequeLinkListDemo.java
@@ -17,13 +17,13 @@ public class DequeLinkListDemo {
 
 
         /*
-                                     Queue	            Deque
-            添加元素到队尾	add(E e) / offer(E e)	addLast(E e) / offerLast(E e)
-            取队首元素并删除	E remove() / E poll()	E removeFirst() / E pollFirst()
-            取队首元素但不删除	E element() / E peek()	E getFirst() / E peekFirst()
-            添加元素到队首	无	                    addFirst(E e) / offerFirst(E e)
-            取队尾元素并删除	无	                    E removeLast() / E pollLast()
-            取队尾元素但不删除	无	                    E getLast() / E peekLast()
+                                     Queue	                Deque
+            添加元素到队尾	        add(E e) / offer(E e)	addLast(E e) / offerLast(E e)
+            取队首元素并删除	        E remove() / E poll()	E removeFirst() / E pollFirst()
+            取队首元素但不删除	    E element() / E peek()	E getFirst() / E peekFirst()
+            添加元素到队首	        无	                    addFirst(E e) / offerFirst(E e)
+            取队尾元素并删除	        无	                    E removeLast() / E pollLast()
+            取队尾元素但不删除	    无	                    E getLast() / E peekLast()
         */
 
         /*
---
+++ advance/src/main/java/com/rjf/collection/StackDemo.java
@@ -0,0 +1,16 @@
+package com.rjf.collection;
+
+/*
+ *
+ *   Rene
+ *   2020/5/24 21:34
+ */
+
+public class StackDemo {
+
+    public static void main(String[] args) {
+
+        String exp = "1 + 2 * ( 9 -5 )";
+
+    }
+}
