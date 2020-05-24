8c3082d01b11f14eef535aa40d63e1a9da2f1400
Rene <1546500177@qq.com>
2020-05-24 21:18:05 +0800

    java基础 - 插入排序

--- advance/src/main/java/com/rjf/algorithms/Insertionsort.java
+++ advance/src/main/java/com/rjf/algorithms/Insertionsort.java
@@ -6,18 +6,28 @@ public class Insertionsort {
 
     public static void main(String[] args) {
 
-        int[] a = {10, 9, 7, 8, 3, 5, 4, 2, 1, 0};
+        int[] ints = {10, 9, 7, 8, 3, 5, 4, 2, 1, 0};
 
-        for (int i = 0; i < a.length; i++) {
-            int temp = a[i];
+        InsertSort(ints);
+
+        System.out.println("排序完毕 !\n"+Arrays.toString(ints));
+    }
+
+    private static void InsertSort(int[] arr){
+        int count = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            int temp = arr[i];
             int j = i - 1;
-            while (j >= 0 && temp <= a[j]) {
-                a[j + 1] = a[j];
+            while (j >= 0 && temp <= arr[j]) {
+                arr[j + 1] = arr[j];
                 j = j - 1;
-            }
-            a[j + 1] = temp;
-            System.out.println(Arrays.toString(a));
-        }
 
+                count++;
+                System.out.println(Arrays.toString(arr)+" O = "+count);
+            }
+            arr[j + 1] = temp;
+
+        }
     }
 }
