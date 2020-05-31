5e1781d72f9005770acfc4b696f96b7a38814805
Rene <1546500177@qq.com>
2020-05-31 15:25:40 +0800

    java基础

--- advance/src/main/java/com/rjf/reg/GroupReg.java
+++ advance/src/main/java/com/rjf/reg/GroupReg.java
@@ -5,28 +5,24 @@ import java.util.regex.Pattern;
 
 public class GroupReg {
     public static void main(String[] args) {
-        Pattern pattern = Pattern.compile("(\\d{3,4})\\-(\\d{7,8})");
-        pattern.matcher("010-12345678").matches(); // true
-        pattern.matcher("021-123456").matches(); // true
-        pattern.matcher("022#1234567").matches(); // false
-        // 获得Matcher对象:
-        Matcher matcher = pattern.matcher("010-12345678");
-        if (matcher.matches()) {
-            String whole = matcher.group(0); // "010-12345678", 0表示匹配的整个字符串
-            String area = matcher.group(1); // "010", 1表示匹配的第1个子串
-            String tel = matcher.group(2); // "12345678", 2表示匹配的第2个子串
-            System.out.println(area);
-            System.out.println(tel);
+        Pattern compile = Pattern.compile("(\\d{4}):(\\d{2}):(\\d{2})");
+        Matcher matcher = compile.matcher("2020:01:01");
+
+        if (matcher.matches()){
+
+                System.out.println(matcher.group(1)+"年");
+                System.out.println(matcher.group(2)+"月");
+                System.out.println(matcher.group(3)+"日");
         }
     }
-
-/*
-
-    正则表达式用(...)分组可以通过Matcher对象快速提取子串：
-
-    group(0)表示匹配的整个字符串；
-    group(1)表示第1个子串，group(2)表示第2个子串，以此类推。
-
-*/
-
 }
+
+    /*
+
+        正则表达式用(...)分组可以通过Matcher对象快速提取子串：
+
+        group(0)表示匹配的整个字符串；
+        group(1)表示第1个子串，group(2)表示第2个子串，以此类推。
+
+    */
+
