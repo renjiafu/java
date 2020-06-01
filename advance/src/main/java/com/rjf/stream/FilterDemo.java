598571b34a793b174288a50b122ed9484106e55d
Rene <1546500177@qq.com>
2020-06-01 20:42:57 +0800

    java基础

---
+++ advance/src/main/java/com/rjf/stream/FilterDemo.java
@@ -0,0 +1,20 @@
+package com.rjf.stream;
+
+/*
+ *
+ *   Rene
+ *   2020/6/1 20:37
+ */
+
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+public class FilterDemo {
+
+    public static void main(String[] args) {
+
+        int[] ints = {1,2,3,4,5,6};
+        IntStream stream = Arrays.stream(ints);
+        stream.filter(i->i%2 == 0).forEach(System.out::println);
+    }
+}
---
+++ advance/src/main/java/com/rjf/stream/ReduceDemo.java
@@ -0,0 +1,18 @@
+package com.rjf.stream;
+
+/*
+ *
+ *   Rene
+ *   2020/6/1 20:40
+ */
+
+import java.util.stream.Stream;
+
+public class ReduceDemo {
+
+    public static void main(String[] args) {
+
+        Integer reduce = Stream.of(1, 2, 3, 4, 5).reduce(0, (sum, n) -> sum + n);
+        System.out.println(reduce);
+    }
+}
