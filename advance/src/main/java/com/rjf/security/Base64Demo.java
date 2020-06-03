58d8f6dc8655103602e9dd04f9a5773aa5f25797
Rene <1546500177@qq.com>
2020-06-03 21:22:43 +0800

    base64

--- advance/src/main/java/com/rjf/security/Base64Demo.java
+++ advance/src/main/java/com/rjf/security/Base64Demo.java
@@ -4,24 +4,23 @@ import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
-import java.util.Arrays;
 import java.util.Base64;
 
-public class EncodeAndDecode {
+public class Base64Demo {
     public static void main(String[] args) throws UnsupportedEncodingException {
 
         String encoded= URLEncoder.encode("中文!", StandardCharsets.UTF_8.toString());
-        System.out.println(encoded);
+        System.out.println("Url en"+encoded);
 
         String decoded= URLDecoder.decode(encoded,StandardCharsets.UTF_8.toString());
-        System.out.println(decoded);
+        System.out.println("Url de : "+decoded);
 
-        byte[] input = new byte[] { (byte) 0xe4, (byte) 0xb8, (byte) 0xad };
-        String b64encoded = Base64.getEncoder().encodeToString(input);
-        System.out.println(b64encoded);
+        byte[] input = "Rene".getBytes();
+        String en = Base64.getEncoder().encodeToString(input);
+        System.out.println("Base64 en: "+en);
 
-        byte[] output = Base64.getDecoder().decode("5Lit");
-        System.out.println(Arrays.toString(output)); // [-28, -72, -83]
+        byte[] output = Base64.getDecoder().decode(en);
+        System.out.println("Base64 de: "+new String(output));
 
 
 
