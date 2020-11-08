81a62646fe444a6d3913d386fdd9ee86bb83cfbe
Rene <1546500177@qq.com>
2020-11-08 23:19:08 +0800

    fmt

--- start/src/main/java/com/rjf/basic/HelloDemo.java
+++ start/src/main/java/com/rjf/basic/HelloDemo.java
@@ -2,6 +2,7 @@ package com.rjf.basic;
 
 public class HelloDemo {
     public static void main(String[] args){
+
         System.out.println("Hello World!");
     }
 }
--- start/src/main/java/com/rjf/basic/IntDemo.java
+++ start/src/main/java/com/rjf/basic/IntDemo.java
@@ -2,6 +2,7 @@ package com.rjf.basic;
 
 public class IntDemo {
     public static void main(String[] args){
+
         int i=(100+200) * (99-88);
         int n=7*(5+(i-9));
         System.out.println(i+":"+n);
