73eb7cbacc1551e8e4f6cbe6434580055b4e5586
Rene <1546500177@qq.com>
2020-11-09 21:02:37 +0800

    基本数据类型

--- start/src/main/java/com/rjf/basic/BasicDataTypesDemo.java
+++ start/src/main/java/com/rjf/basic/BasicDataTypesDemo.java
@@ -10,18 +10,21 @@ public class BasicDataTypesDemo {
 
     public static void main(String[] args) {
 
-        byte byte1 = -128;
-        byte byte2 = 127;
+        byte byte1 = -128; // -2^7
+        byte byte2 = 127;   // 2^7-1
 
-        short short1 = -32768;
-        short short2 = 32767;
+        short short1 = -32768; // -2^15
+        short short2 = 32767;   // 2^15-1
 
-        int int1 = 2147483647;
-        int int2 = -2147483648;
+        int int1 = 2147483647;  // 2^31-1
+        int int2 = -2147483648; // -2^31
         int int3 = 2_000_000_000; // 加下划线更容易识别
         int int4 = 0xff0000; // 十六进制表示的16711680
         int int5 = 0b1111_0000; // 二进制表示的240
-        long l1 = 9000000000000000000L; // long型的结尾需要加L
+
+        // long型的结尾需要加L
+        long l1 = 9223372036854775807L; // 2^63 -1
+        long l2 = -9223372036854775808L; // -2^63
 
         float f1 = 3.14f;
         float f2 = 3.14e38f; // 科学计数法表示的3.14x10^38
@@ -44,9 +47,9 @@ public class BasicDataTypesDemo {
 
                 Java定义了8种基本数据类型：
 
-                整数类型：byte，short，int，long
+                整数类型：byte，short，int，long    (补码表示)
 
-                浮点数类型：float，double
+                浮点数类型：float，double          (IEEE 754标准)
 
                 字符类型：char
 
