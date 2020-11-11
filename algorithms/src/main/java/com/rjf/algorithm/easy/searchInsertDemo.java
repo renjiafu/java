7a1b5d9e99f59665c34d0088fecd35261d690440
Rene <1546500177@qq.com>
2020-11-11 22:10:58 +0800

    二分

--- algorithms/src/main/java/com/rjf/algorithm/easy/searchInsertDemo.java
+++ algorithms/src/main/java/com/rjf/algorithm/easy/searchInsertDemo.java
@@ -10,29 +10,46 @@ package com.rjf.algorithm.easy;
 public class searchInsertDemo {
 
     public static void main(String[] args) {
+        int[] ints = {1, 3, 5, 7};
+        int target = 6;
 
-        int[] ints = {1,3,5,7};
-        int target =6;
-
-        System.out.println(searchInsert(ints,target)); // 3
+        // 3
+        System.out.println(searchInsert(ints, target));
+        System.out.println(searchInset2(ints, target));
     }
 
     // 顺序遍历
     public static int searchInsert(int[] nums, int target) {
 
         // 1.假定在target在数组中
-        for(int i=0; i<nums.length; i++){
-            if(nums[i]==target){
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == target) {
                 return i;
             }
         }
 
         // 2.target不在数组中
-        for(int i=0; i<nums.length; i++){
-            if(nums[i]>target){
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > target) {
                 return i;
             }
         }
         return nums.length;
+    }
+
+    //  二分法
+    public static int searchInset2(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length;
+
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] > target) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return left;
     }
 }
