9b37f253c993f04868501d7c675c0f38717f5301
Rene <1546500177@qq.com>
2021-03-01 23:44:20 +0800

    更新

--- oop/src/main/java/com/rjf/oop/function/FunctionInterface.java
+++ oop/src/main/java/com/rjf/oop/function/FunctionInterface.java
@@ -2,8 +2,9 @@ package com.rjf.oop.function;
 
 /**
  *  函数式接口
- *  des:有且仅有一个抽象方法（延迟至lambda表达式or方法引用实现）
- *  1.给此抽象方法定义出入参，方法名
+ *  des:
+ *  特点：有且仅有一个抽象方法（延迟至lambda表达式or方法引用实现）
+ *  1.给此接口仅有的一个抽象方法定义出入参，方法名
  */
 
 @FunctionalInterface
